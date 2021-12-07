27189af8c53a3e3f0b4ed63f0624b9cc193b5c21
Alice <aliwen@softwire.com>
2021-12-07 09:39:05 +0000

    Day7- complete

---
+++ src/Day7-Whales.py
@@ -0,0 +1,45 @@
+def read_in_postions(file_name):
+    with open(file_name) as file:
+        data = file.readline()
+        return [int(n) for n in data.rstrip().split(",")]
+
+def get_score_for_n_linier(positions, n):
+    positions_diff = map(lambda p: abs(p-n), positions)
+    fuel = sum(positions_diff)
+    return fuel
+
+def get_score_for_n_exp(positions, n):
+    position_costs = map(lambda p: sum(range(0,abs(p-n)+1)), positions)
+    fuel = sum(position_costs)
+    return fuel
+
+
+def day_seven_part_one(test=False):
+    file_name = "data/day_seven_test.txt" if test else "data/day_seven.txt"
+
+    positions = read_in_postions(file_name)
+
+    min_pos, max_pos = (min(positions), max(positions))
+
+    fuel_use = [get_score_for_n_linier(positions,i) for i in range(min_pos, max_pos+1)]
+
+    return(min(fuel_use))
+
+def day_seven_part_two(test=False):
+    file_name = "data/day_seven_test.txt" if test else "data/day_seven.txt"
+
+    positions = read_in_postions(file_name)
+
+    min_pos, max_pos = (min(positions), max(positions))
+
+    fuel_use = [get_score_for_n_exp(positions,i) for i in range(min_pos, max_pos+1)]
+
+    return(min(fuel_use))
+
+
+
+    
+
+
+# print(day_seven_part_one(test=False))
+print(day_seven_part_two(test=False))
